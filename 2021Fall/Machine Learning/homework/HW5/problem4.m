685f30936b60f1b25f90b4c4bbaff88e18a0d544
Yinhong Qin <yq2021@nyu.edu>
2022-09-11 15:02:22 -0400

    feat: Add CSA slide1, hw 1, textbooks

--- 2021Fall/Machine Learning/homework/HW5/problem4.m
+++ 2021Fall/Machine Learning/homework/HW5/problem4.m
@@ -4,3 +4,10 @@ p_test{2} = [0.5, 0.1; 0.1, 0.5];
 p_test{3} = [0.1, 0.5; 0.5, 0.1];
 p_test{4} = [0.9, 0.3; 0.1, 0.3];
 [m_test, m_origin] = junction_tree(p_test);
+
+
+plot(x, y);
+grid on;
+xlabel("Percent Encryption");
+ylabel("Net Speedup");
+xtickformat("percentage");
